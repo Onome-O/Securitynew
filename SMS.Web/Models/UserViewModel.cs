e396f3942201afd12af563ccf829a7eac4abc857
Onome-O <126192498+Onome-O@users.noreply.github.com>
2023-04-23 22:06:58 +0000

    new commit

--- SMS.Web/Models/UserViewModel.cs
+++ SMS.Web/Models/UserViewModel.cs
@@ -6,5 +6,23 @@ namespace SMS.Web.Models;
 public class UserViewModel
 {       
     // TBC add attributes
+    [Required]
+    public string Name { get; set; }
+    
+    Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "Passwords don't match!")]
+    [Display(Name = "Confirm Password")]
+
+    [Required]
+    public
+
+    
+  
 
 }
